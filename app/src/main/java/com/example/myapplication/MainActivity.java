bc43a7fd99cf74d368ee24282d9a43d03bcfdab1
Nicu <nicu_usvat@yahoo.com>
2022-10-08 15:05:00 +0300

    Task 1

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,7 +2,13 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.myapplication.lifecycle.ActivityA;
+import com.example.myapplication.lifecycle.ActivityB;
+import com.example.myapplication.lifecycle.ActivityC;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void clicked(View view){
+        switch (view.getId()){
+            case R.id.buttonA:
+                startActivity(new Intent(this, ActivityA.class));
+                break;
+            case R.id.buttonB:
+                startActivity(new Intent(this, ActivityB.class));
+                break;
+            case R.id.buttonC:
+                startActivity(new Intent(this, ActivityC.class));
+                break;
+        }
     }
 }
